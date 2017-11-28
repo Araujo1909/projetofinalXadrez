db2a5281baefaec7cbd39df07b173355e4931119
Mateus Flausino <mateusaraujo1909@gmail.com>
2017-11-28 09:02:29 -0200

    Projeto iniciado

---
+++ trabalhofinal/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace trabalhofinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá");
+            Console.ReadLine();
+        }
+    }
+}
